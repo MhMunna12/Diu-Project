964e781e918f248052e5e581347aec999a9441a2
Monwar Hossain <monwarhossainmunna@gmail.com>
2021-10-14 20:13:52 +0600

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//middleware
+const app = express();
+app.use(bodyParser.json());
 const port = 7070
+
+const password = 'carUsermunna'
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://carUser:carUsermunna@cluster0.nwuix.mongodb.net/servicecar?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("servicecar").collection("addroute");
+  console.log('database connection');
+  // client.close();
+});
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
